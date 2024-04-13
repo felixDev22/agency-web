8bd3f1f7d6b4fe061710fa243d7430f24773b1df
felixDev22 <felixnzomo@gmail.com>
2024-04-13 03:53:45 +0300

    Add Navbar file, create the navlist and add mobile Icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import { navLinks } from '../constants/constData';
+import styles from '../constants/styles';
+import { menu, close } from '../assets';
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className={`${styles.nav}`}>
+      <h3 className="text-white font-bold text-[32px]">Dooh</h3>
+      <ul className={`${styles.mobileUl}`}>
+        {navLinks.map((nav, index) => (
+          <li className={`${styles.li}`} key={index}>
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className={`${styles.menuToggle}`}>
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className={`${styles.menuIcon}`}
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${toggle ? 'flex' : 'hidden'} ${
+            styles.mobileMenu
+          } `}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
