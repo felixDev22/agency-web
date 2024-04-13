2a2a8e1ee4f91f39f8f743237a3041f50ca18e32
felixDev22 <felixnzomo@gmail.com>
2024-04-13 03:32:23 +0300

    Create a styles files to organize all styles and resuse styles

---
+++ src/constants/styles.js
@@ -0,0 +1,29 @@
+const styles = {
+  nav: 'navbar w-full flex py-6 justify-between items-center',
+  mobileUl: 'list-none sm:flex flex-1 hidden justify-end items-center',
+  li: 'font-poppins text-[16px] text-white ml-10 cursor-pointer',
+  menuToggle: 'sm:hidden flex flex-1 justify-end items-center',
+  menuIcon: 'w-[28px] h-[28px] object-contain',
+  mobileMenu:
+    'min-w-[140px] absolute top-20 mx-4 my-2 p-6 rounded sidebar bg-black-gradient',
+  containerWidth: 'xl:max-w-[1280px] w-full',
+
+  heading2:
+    'font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full',
+  paragraph:
+    'font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]',
+
+  flexCenter: 'flex justify-center items-center',
+  flexStart: 'flex justify-center items-start',
+
+  paddingX: 'sm:px-16 px-6',
+  paddingY: 'sm:py-16 py-6',
+  padding: 'sm:px-16 px-6 sm:py-12 py-4',
+
+  marginX: 'sm:mx-16 mx-6',
+  marginY: 'sm:my-16 my-6',
+};
+
+
+
+export default styles;
