5acb7eed94b1511bd3517d7309bfab7c9a7d7480
felixDev22 <felixnzomo@gmail.com>
2024-04-17 23:56:02 +0300

    Create data file to hold all the content /info

---
+++ src/constants/data.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'products',
+    title: 'Products',
+  },
+  {
+    id: 'contacts',
+    title: 'Contacts',
+  },
+];
