36c82ccebb781caf6920e404fcaa3c7e70f8bbf9
felixDev22 <felixnzomo@gmail.com>
2024-04-20 23:31:46 +0300

    Create the hero section. Import the styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import styles from '../constants/styles';
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className={`${styles.heroWrapper} ${styles.paddingY}`}>
+      Hero
+    </section>
+  );
+};
+
+export default Hero;
