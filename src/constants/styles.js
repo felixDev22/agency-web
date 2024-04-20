0f7ed12c6ce846d00a8dfb75aa745138231d814b
felixDev22 <felixnzomo@gmail.com>
2024-04-20 23:32:56 +0300

    Update the styles add hero styling

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -12,7 +12,11 @@ const styles = {
     'min-w-[140px] absolute top-20 mx-4 my-2 p-6 rounded sidebar bg-black-gradient',
   containerWidth: 'xl:max-w-[1280px] w-full',
 
-  heading2:
+  heroWrapper: 'flex md:flex-row flex-col items-center bg-slate-400 w-[580px] h=[30px]',
+  heroInner: 'flex-1 flex-col xl:px-0 sm:px-15 px=5',
+  titleWrapper: 'flex flex-row justify-between items-center w-full',
+
+  heading1:
     'font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full',
   paragraph:
     'font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]',
