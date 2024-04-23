11ff3468e30c0302343e9d43035e28a4921aa66a
felixDev22 <felixnzomo@gmail.com>
2024-04-23 23:58:53 +0300

    Update the name from costData to content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-import { navLinks } from '../constants/constData';
+import { navLinks } from '../constants/content';
 import styles from '../constants/styles';
 import { menu, close } from '../assets';
 
