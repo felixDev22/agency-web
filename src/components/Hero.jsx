d73c213a30897fd3bd4c2ed965a7c0f4670a9d20
felixDev22 <felixnzomo@gmail.com>
2024-04-25 15:34:40 +0300

    Add header title with a gradent on Digital span

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import styles from '../constants/styles';
+import { heroPic } from '../assets';
 
 const Hero = () => {
   return (
-    <section
-      id="home"
-      className={`${styles.heroWrapper} ${styles.paddingY}`}>
-      Hero
+    <section id="home" className={`${styles.heroWrapper} ${styles.paddingY}`}>
+      <div className={`${styles.titleWrapper}`}>
+        <h1 className={`${styles.h1}`}>
+          SIMPLE & FAST<br></br>
+          <span className={`${styles.h1Gradient}`}>DIGITAL </span>
+          PAYMENT<br></br>SOLUTIONS
+        </h1>
+      </div>
+      <div className={`${styles.heroImage}`}>
+        <img src={heroPic} className="w-[700px] h-auto" />
+      </div>
     </section>
   );
 };
