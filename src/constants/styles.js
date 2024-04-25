2afe45bb25c331c21ea1461ed1738071f7f223de
felixDev22 <felixnzomo@gmail.com>
2024-04-25 16:11:06 +0300

    Update titleWrapper and paragraph. add width

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -14,15 +14,15 @@ const styles = {
 
   heroWrapper: 'flex md:flex-row flex-col items-center',
   heroInner: 'flex-1 flex-col xl:px-0 sm:px-15 px=5',
-  titleWrapper: 'flex flex-row justify-between items-center w-full h-auto px-6',
+  titleWrapper: 'flex flex-col w-full h-auto px-6',
   heroImage: 'flex flex-row justify-between items-center',
 
   h1: 'flex-1 font-poppins font-semibold ss:text-[60px] text-[50px] text-white',
   h1Gradient: 'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-2',
   heading1:
     'font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full',
-  paragraph:
-    'font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]',
+  paragraph: 'font-poppins font-normal text-white text-[18px] leading-[30.8px]',
+  pWidth: 'w-[450px] md:w-[600px] ',
 
   flexCenter: 'flex justify-center items-center',
   flexStart: 'flex justify-center items-start',
