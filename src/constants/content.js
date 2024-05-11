bf13685d8fcf6bbbdf6c78d8e41eb1ed506e668e
felixDev22 <felixnzomo@gmail.com>
2024-05-11 23:37:53 +0300

    Add info details to the content

--- src/constants/content.js
+++ src/constants/content.js
@@ -12,3 +12,22 @@ export const navLinks = [
     title: 'Contacts',
   },
 ];
+
+
+export const info = [
+{
+  id: 1,
+  desc: 'Active User',
+  rates: '5000+'
+},
+{
+  id: 2,
+  desc: 'Companies',
+  rates: '640+',
+},
+{
+  id: 2,
+  desc: 'Transactions',
+  rates: '$130M+',
+},
+];
