6ee7797248ff81a180ddc97441721cd8a7dac9ea
felixDev22 <felixnzomo@gmail.com>
2024-05-11 23:43:15 +0300

    Update the App with the Hero and Info components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Navbar, Hero } from './components';
+import styles from './constants/styles';
+import Info from './components/Info';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden ">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.containerWidth}`}>
+          <Navbar />
+        </div>
+      </div>
 
-export default App
+      <div className={` bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.containerWidth}`}>
+          <Hero />
+          <Info />
+        </div>
+      </div>
+
+      <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.containerWidth}`}>
+          stats business billing CardDeal Testimonials client create footer
+        </div>
+      </div>
+    </div>
+  );
+};
+export default App;
