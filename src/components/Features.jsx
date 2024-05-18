df9a120c8e6972d73571421d19a802e72f391d7c
felixDev22 <felixnzomo@gmail.com>
2024-05-18 17:33:33 +0300

    Add Features, import styling

---
+++ src/components/Features.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../constants/styles'
+
+const Features = () => {
+  return (
+    <section id="products" className={styles.card}>
+      <div className={styles.cardDetails}>
+        <h2 className={styles.heading2}>
+          The best solutions
+          <br></br>for your business
+        </h2>
+        <p></p>
+      </div>
+    </section>
+  );
+}
+
+export default Features
