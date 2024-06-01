db952387b2309f10093bd666cb7830abd9037bb0
felixDev22 <felixnzomo@gmail.com>
2024-06-01 14:14:32 +0300

    Update the pics

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import menu from './menu.png';
 import close from './close.png';
+import heroPic from './hero-img.png';
+import bizMan from './biz-man.png'
 
-export { menu, close };
+export { menu, close, heroPic, bizMan };
