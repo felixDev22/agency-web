c617e802bfe98d248dfaff515ff8ec42880fe124
felixDev22 <felixnzomo@gmail.com>
2024-06-01 14:11:11 +0300

    Create the product object tat can be easily updated.

--- src/constants/content.js
+++ src/constants/content.js
@@ -13,21 +13,38 @@ export const navLinks = [
   },
 ];
 
-
 export const info = [
-{
-  id: 1,
-  desc: 'Active User',
-  rates: '5000+'
-},
-{
-  id: 2,
-  desc: 'Companies',
-  rates: '640+',
-},
-{
-  id: 2,
-  desc: 'Transactions',
-  rates: '$130M+',
-},
+  {
+    id: 1,
+    desc: 'Active User',
+    rates: '5000+',
+  },
+  {
+    id: 2,
+    desc: 'Companies',
+    rates: '640+',
+  },
+  {
+    id: 2,
+    desc: 'Transactions',
+    rates: '$130M+',
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    title: '1st Feature',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  },
+  {
+    id: 2,
+    title: '2nd Feature',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  },
+  {
+    id: 3,
+    title: '3rd Feature',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  },
 ];
