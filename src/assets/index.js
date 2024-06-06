cf953593446a2708a594fbbccfd4575454a4870e
felixDev22 <felixnzomo@gmail.com>
2024-06-06 21:48:56 +0300

    Add phone image

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import menu from './menu.png';
 import close from './close.png';
 import heroPic from './hero-img.png';
 import bizMan from './biz-man.png'
+import phone from './mobile-phone.png'
 
-export { menu, close, heroPic, bizMan };
+export { menu, close, heroPic, bizMan, phone};
