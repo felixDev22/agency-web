946f30597f7237d15beb7a4db5dc4f7cf22d5ea7
felixDev22 <felixnzomo@gmail.com>
2024-06-07 13:39:48 +0300

    Update product and about content

--- src/constants/content.js
+++ src/constants/content.js
@@ -34,17 +34,35 @@ export const info = [
 export const products = [
   {
     id: 1,
-    title: '1st Feature',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    title: 'Innovative Technology',
+    desc: 'Leveraging the latest advancements to provide cutting-edge payment solutions.',
   },
   {
     id: 2,
-    title: '2nd Feature',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    title: 'Reward Programs',
+    desc: 'Enjoy exclusive rewards and benefits tailored to your usage and needs.',
   },
   {
     id: 3,
-    title: '3rd Feature',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    title: 'Global Reach',
+    desc: 'Facilitate transactions across borders with ease, expanding your business potential.',
+  },
+];
+
+export const About = [
+  {
+    id: 2,
+    title: '24/7 Support',
+    desc: 'Our dedicated customer support team is available around the clock to assist you.',
+  },
+  {
+    id: 2,
+    title: 'Enhanced Security',
+    desc: 'Our top-tier security protocols ensure your transactions are safe and protected from fraud.',
+  },
+  {
+    id: 3,
+    title: 'User-Friendly Interface:',
+    desc: 'Easy-to-use platform designed for a seamless user experience.',
   },
 ];
