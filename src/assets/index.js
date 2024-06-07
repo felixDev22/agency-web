1fab4368ef96d0dfebed2d739c486304fc04f799
felixDev22 <felixnzomo@gmail.com>
2024-06-07 13:47:18 +0300

    Add playstore and applestore icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import menu from './menu.png';
 import close from './close.png';
 import heroPic from './hero-img.png';
-import bizMan from './biz-man.png'
-import phone from './mobile-phone.png'
+import bizMan from './biz-man.png';
+import phone from './mobile-phone.png';
+import playstore from './Google Play.png';
+import applestore from './App store.png';
 
-export { menu, close, heroPic, bizMan, phone};
+export { menu, close, heroPic, bizMan, phone, playstore, applestore };
