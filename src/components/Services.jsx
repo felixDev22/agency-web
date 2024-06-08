ab0789f9433dfc922dc81f1aa818fb4711c832de
felixDev22 <felixnzomo@gmail.com>
2024-06-09 00:49:37 +0300

    Add hover effect with a slight scale increment

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,13 +7,15 @@ const Services = () => {
       <h2 className={`${styles.heading2} mb-8`}>
         Our services are designed for<br></br>businesses of all sizes
       </h2>
-      <div className="w-300px h-300px flex md:flex-row flex-col gap-8">
+      <div className="w-300px h-300px flex md:flex-row flex-col gap-8 ">
         {services.map((service, index) => (
-          <div key={index}>
-            <div>
-              <p className={`${styles.pbold}`}>{service.title}</p>
+          <div
+            key={index}
+            className="relative transition-transform duration-300 hover:scale-110">
+            <img src={service.image} alt="servi" className="rounded-[20px] " />
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-indigo-600 to-transparent p-4 rounded-b-[20px]">
+              <p className={`${styles.pbold} text-white`}>{service.title}</p>
             </div>
-            <img src={service.image} className="rounded-[20px] " />
           </div>
         ))}
       </div>
