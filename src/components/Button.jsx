90f8f67f63b48a7ddb36f1a1b07e3141313093cb
felixDev22 <felixnzomo@gmail.com>
2024-06-10 22:56:55 +0300

    Add transition effect for the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,9 @@ import { MdArrowForward } from 'react-icons/md';
 const Button = ({prop}) => {
   return (
     <button
-      className={`${styles.button} flex items-center justify-center gap-2`}>
-      {prop}<MdArrowForward />
+      className={`${styles.button} flex items-center justify-center gap-2 transform transition-transform duration-300 hover:scale-105`}>
+      {prop}
+      <MdArrowForward />
     </button>
   );
 }
